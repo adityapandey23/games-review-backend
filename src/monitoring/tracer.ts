a20875f255eeec339b623fd42fea9e7b229ffd91
Aditya Pandey <adityapandey2395@gmail.com>
2026-03-27 19:11:37 +0530

    nevermind I am stupid

--- src/monitoring/tracer.ts
+++ src/monitoring/tracer.ts
@@ -3,7 +3,7 @@ import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentation
 import { JaegerExporter } from "@opentelemetry/exporter-jaeger";
 
 const observabilityIp = process.env.OBSERVABILITY_VM_IP || "localhost";
-const endpoint = process.env.JAEGER_ENDPOINT || `http://${observabilityIp}:16686/api/traces`;
+const endpoint = process.env.JAEGER_ENDPOINT || `http://${observabilityIp}:14268/api/traces`;
 
 const sdk = new NodeSDK({
   traceExporter: new JaegerExporter({
